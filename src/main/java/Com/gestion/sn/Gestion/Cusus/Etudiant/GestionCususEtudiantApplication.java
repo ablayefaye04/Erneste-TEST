5acbdf91de62750e8f658200aba519e396a04a1d
ablayefaye04 <fayeablaye404@gmail.com>
2025-04-12 10:21:34 +0000

    Initialisation du projet

---
+++ src/main/java/Com/gestion/sn/Gestion/Cusus/Etudiant/GestionCususEtudiantApplication.java
@@ -0,0 +1,13 @@
+package Com.gestion.sn.Gestion.Cusus.Etudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCususEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCususEtudiantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/gestion/sn/Gestion/Cusus/Etudiant/GestionCususEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.gestion.sn.Gestion.Cusus.Etudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCususEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
